0d83be33f619fb1b607a068a2ab20013ed9fd792
babyazalea <usun12@hotmail.com>
2021-09-08 23:29:40 +0900

    styling

---
+++ src/content/blog/third/index.md
@@ -0,0 +1,7 @@
+---
+title: third
+date: "2021-09-05T22:12:03.284Z"
+category: "all"
+---
+
+This is my first test
--- src/content/portfolios/portfolios-data.js
+++ src/content/portfolios/portfolios-data.js
@@ -6,4 +6,18 @@ export const portfoliosData = [
     "theme-color": "var(--amber-color800)",
     emoji: "🍔",
   },
+  {
+    title: "햄버거 찾기",
+    description: "원하는 햄버거를 찾아드려요",
+    skills: ["react", "firebase", "angular"],
+    "theme-color": "var(--amber-color800)",
+    emoji: "🍔",
+  },
+  {
+    title: "햄버거 찾기",
+    description: "원하는 햄버거를 찾아드려요",
+    skills: ["react", "firebase", "angular"],
+    "theme-color": "var(--amber-color800)",
+    emoji: "🍔",
+  },
 ];
