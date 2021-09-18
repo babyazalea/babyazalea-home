fac522db793ad63ff2c09e44e5d6f826186f9d74
babyazalea <usun12@hotmail.com>
2021-09-19 02:11:49 +0900

    added first-lines

--- src/components/home/introduction/Introduction.js
+++ src/components/home/introduction/Introduction.js
@@ -1,33 +1,55 @@
 import React from "react";
+import { hadSkills } from "../../../content/portfolios/portfolios-data";
 
 import Card from "../../ui/card/Card";
+import SkillIcons from "../../ui/skill-icon/SkillIcons";
 import "./Introduction.scss";
 
 const Introduction = () => {
+  const skills = hadSkills();
+
   return (
     <div className="introduction">
-      <div className="one">
+      <div className="intro-box one">
+        <Card customClassName="intro-card">
+          <div>
+            <span>babyazalea's</span>
+            <br />
+            <span>home</span>
+          </div>
+        </Card>
+      </div>
+      <div className="intro-box two">
+        <Card customClassName="intro-card">
+          <div>
+            <span>홈페이지 사용법</span>
+            <span>README</span>
+          </div>
+        </Card>
+      </div>
+      <div className="intro-box three">
+        <Card customClassName="intro-card">
+          <div className="portfolio-nav-logo-part">
+            <SkillIcons skills={skills} />
+          </div>
+          <div className="portfolio-nav-text-part">
+            <span>포트폴리오</span>
+          </div>
+        </Card>
+      </div>
+      <div className="intro-box four">
         <Card customClassName="intro-card"></Card>
       </div>
-      <div className="two">
+      <div className="intro-box five">
         <Card customClassName="intro-card"></Card>
       </div>
-      <div className="three">
+      <div className="intro-box six">
         <Card customClassName="intro-card"></Card>
       </div>
-      <div className="four">
+      <div className="intro-box seven">
         <Card customClassName="intro-card"></Card>
       </div>
-      <div className="five">
-        <Card customClassName="intro-card"></Card>
-      </div>
-      <div className="six">
-        <Card customClassName="intro-card"></Card>
-      </div>
-      <div className="seven">
-        <Card customClassName="intro-card"></Card>
-      </div>
-      <div className="eight">
+      <div className="intro-box eight">
         <Card customClassName="intro-card"></Card>
       </div>
     </div>
