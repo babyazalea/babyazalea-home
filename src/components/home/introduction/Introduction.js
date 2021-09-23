306f34150b81e366e6788533f59d855ccb2e163f
babyazalea <usun12@hotmail.com>
2021-09-23 11:59:43 +0900

    nav-resume

--- src/components/home/introduction/Introduction.js
+++ src/components/home/introduction/Introduction.js
@@ -45,7 +45,61 @@ const Introduction = () => {
         <Card customClassName="intro-card"></Card>
       </div>
       <div className="intro-box five">
-        <Card customClassName="intro-card"></Card>
+        <Card customClassName="intro-card">
+          <div className="resume-wrapper">
+            <div className="resume-title">
+              <span>RESUME</span>
+            </div>
+            <div className="resume-visual">
+              <div className="blah-blah">
+                <div className="picture-frame"></div>
+                <span>
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters,
+                </span>
+                <span>
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters,
+                </span>
+                <span>
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters,
+                </span>
+                <span>
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters,
+                </span>
+                <span>
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters,
+                </span>
+                <span>
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters,
+                </span>
+                <span>
+                  as opposed to using 'Content here, content here', making it
+                  look like readable English.
+                </span>
+                <span>
+                  Many desktop publishing packages and web page editors now use
+                  Lorem Ipsum as their default model text,
+                </span>
+                <span>
+                  and a search for 'lorem ipsum' will uncover many web sites
+                  still in their infancy.
+                </span>
+                <span>
+                  Various versions have evolved over the years, sometimes by
+                  accident,
+                </span>
+                <span>
+                  sometimes on purpose (injected humour and the like).
+                </span>
+              </div>
+            </div>
+          </div>
+        </Card>
       </div>
       <div className="intro-box six">
         <Card customClassName="intro-card"></Card>
