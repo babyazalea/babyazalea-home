00970da94ae17f67fe7b3e0d528dbe1a50a41d7a
babyazalea <usun12@hotmail.com>
2021-12-04 03:10:42 +0900

    fixing gatsby transformer remark config

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,26 +36,6 @@ module.exports = {
               aliases: {},
               showLineNumbers: false,
               noInlineHighlight: false,
-              languageExtensions: [
-                {
-                  language: "superscript",
-                  extend: "javascript",
-                  definition: {
-                    superscript_types: /(SuperType)/,
-                  },
-                  insertBefore: {
-                    function: {
-                      superscript_keywords: /(superif|superelse)/,
-                    },
-                  },
-                },
-              ],
-              prompt: {
-                user: "root",
-                host: "localhost",
-                global: false,
-              },
-              escapeEntities: {},
             },
           },
           {
