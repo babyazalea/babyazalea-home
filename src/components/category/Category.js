7e0ef2d2b89e612a13d3a897458b1c7f3653b927
babyazalea <usun12@hotmail.com>
2021-12-10 01:38:22 +0900

    adjust category

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -5,20 +5,20 @@ import "./Category.scss";
 const Category = (props) => {
   return (
     <div className="category">
+      <button
+        className="reading-all"
+        onClick={() => props.categoryInitializer()}
+      >
+        <i className="far fa-folder"></i>
+        모든 글
+      </button>
       <ul className="category-wrapper">
-        <button
-          className="reading-all"
-          onClick={() => props.categoryInitializer()}
-        >
-          <i className="far fa-folder"></i>
-          모든 글
-        </button>
         {props.subCategorys.map((category, index) => {
           return (
             <li key={index} className="sub-category">
               <button onClick={() => props.categoryHandler(category)}>
                 <i className="far fa-folder"></i>
-                {category}
+                <span>{category}</span>
               </button>
             </li>
           );
