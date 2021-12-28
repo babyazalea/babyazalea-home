8f9db5e364c50ac8dab3c88e4881a7ed4549786e
babyazalea <usun12@hotmail.com>
2021-12-28 13:00:44 +0900

    new frame

--- src/components/portfolio-list/portfolio/Portfolio.js
+++ src/components/portfolio-list/portfolio/Portfolio.js
@@ -17,29 +17,44 @@ const Portfolio = (props) => {
 
   return (
     <div className="portfolio">
-      <Card
-        customClassName="portfolio-card"
-        bgStyle={props.portfolio["color-theme"].card}
-      >
-        <div className="portfolio-inside" style={insideBg}>
-          <div className="portfolio-title" style={titleAndDescBg}>
-            <span>{props.portfolio.title}</span>
-            <div className="show-me-what-you-got">
-              <SkillIcons skills={props.portfolio.skills} />
-            </div>
-          </div>
-          <div className="emoji">{props.portfolio.emoji}</div>
-          <div className="portfolio-description" style={titleAndDescBg}>
-            <span>{props.portfolio.description}</span>
-          </div>
-          <div className="links">
-            <a className="play-link" href={props.portfolio.playLink}>
-              <i className="fas fa-play"></i>
-            </a>
-            <a className="github-link" href={props.portfolio.githubLink}>
-              <i className="fab fa-github"></i>
-            </a>
-          </div>
+      {/*<Card*/}
+      {/*  customClassName="portfolio-card"*/}
+      {/*  bgStyle={props.portfolio["color-theme"].card}*/}
+      {/*>*/}
+      {/*  <div className="portfolio-inside" style={insideBg}>*/}
+      {/*    <div className="portfolio-title" style={titleAndDescBg}>*/}
+      {/*      <span>{props.portfolio.title}</span>*/}
+      {/*      <div className="show-me-what-you-got">*/}
+      {/*        <SkillIcons skills={props.portfolio.skills} />*/}
+      {/*      </div>*/}
+      {/*    </div>*/}
+      {/*    <div className="emoji">{props.portfolio.emoji}</div>*/}
+      {/*    <div className="portfolio-description" style={titleAndDescBg}>*/}
+      {/*      <span>{props.portfolio.description}</span>*/}
+      {/*    </div>*/}
+      {/*    <div className="links">*/}
+      {/*      <a className="play-link" href={props.portfolio.playLink}>*/}
+      {/*        <i className="fas fa-play"></i>*/}
+      {/*      </a>*/}
+      {/*      <a className="github-link" href={props.portfolio.githubLink}>*/}
+      {/*        <i className="fab fa-github"></i>*/}
+      {/*      </a>*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*</Card>*/}
+      <Card>
+        <span>{props.portfolio.title}</span>
+        <span>{props.portfolio.description}</span>
+        <div className="show-me-what-you-got">
+          <SkillIcons skills={props.portfolio.skills} />
+        </div>
+        <div className="links">
+          <a className="play-link" href={props.portfolio.playLink}>
+            <i className="fas fa-play"></i>
+          </a>
+          <a className="github-link" href={props.portfolio.githubLink}>
+            <i className="fab fa-github"></i>
+          </a>
         </div>
       </Card>
     </div>
